97b914a9ba7ec6093277badc35a0c886da12603c
Gihyeon-Kwon <rlg1133@naver.com>
2021-02-18 03:18:37 +0900

    add text widget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -7,7 +7,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      title: "First App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue// 테마 기본 색상이 블루라는 의미
+      ),
+    home : MyHomePage(),
+    );
+  }
+}
+
+//원래 내부에 데이터가 바뀌는 부분이 하나라도 있으면 스테이트풀로 해야하는데 지금은 스테이트리스
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(//발판을 만들어준다는 의미
+      appBar: AppBar(
+        title: Text("First App"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('Hello'),
+            Text('Hello'),
+            Text('Hello')
+          ],
+        ),
+      ),
     );
   }
 }
