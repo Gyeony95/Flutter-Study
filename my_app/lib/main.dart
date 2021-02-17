a3716aef517c0d5ddb03c94b946463eb70687338
Gihyeon-Kwon <rlg1133@naver.com>
2021-02-18 02:58:33 +0900

    create my app

---
+++ my_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// 메인 함수 실행하면 최상위 함수인 런앱 실행, 런앱 안에는 위젯이 있어야함
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
