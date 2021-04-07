0d04c7368dd6f123065ceedc54b725fd91533798
Gihyeon-Kwon <rlg1133@naver.com>
2021-04-07 21:08:57 +0900

    base code

---
+++ navi_mom/lib/route/navigator_manager.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NavigatorManager {
+  factory NavigatorManager() => _instance;
+
+  static final _instance = NavigatorManager.private();
+
+  GlobalKey<NavigatorState> _navigatorKey;
+  GlobalKey<NavigatorState> get navigatorKey => _navigatorKey;
+
+  RouteObserver<PageRoute> _routeObserver;
+  RouteObserver<PageRoute> get routeObserver => _routeObserver;
+
+  NavigatorManager.private() {
+    _navigatorKey = GlobalKey();
+    // _routeObserver = FirebaseAnalyticsObserver(
+    //   analytics: FirebaseAnalytics(),
+    // );
+  }
+}
