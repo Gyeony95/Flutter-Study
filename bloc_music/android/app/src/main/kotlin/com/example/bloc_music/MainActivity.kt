5b89dc8a9d86b67c2360d1cbf7f245ccf12bea47
권기현 <gwongihyeon@gwongihyeon-ui-MacBookAir.local>
2021-04-09 16:26:35 +0900

    recommit

--- bloc_music/android/app/src/main/kotlin/com/example/bloc_music/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bloc_music
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bloc_music/lib/bloc/main_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'main_bloc.dart';
-
-abstract class MainEvent extends Equatable{
-  @override
-  List<Object> get props => [];
-}
-class GetMusicList extends MainEvent {}
