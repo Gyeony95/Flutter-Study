1b9cfc8eaa4597739d1e6a805db58ccf39e3d942
Gihyeon-Kwon <rlg1133@naver.com>
2021-05-19 16:23:57 +0900

    add chart view

--- gyeony_diary/lib/screen/main_screen.dart
+++ gyeony_diary/lib/screen/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 import 'package:get/get.dart';
+import 'package:gyeony_diary/widget/manu_widget.dart';
 
 
 class MainScreen extends StatefulWidget {
@@ -11,23 +13,38 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('일기를 써보아요'),
-      ),
-      body:  Container(
-          child: Column(
-              children: [
-                Text('작성된 일기가 없습니다!'),
-                Spacer(),
-                Padding(
-                  padding: EdgeInsets.all(30),
-                  child: InkResponse(
-                    child: Center(child: Text('여기를 눌러 일기를 써보세요!')),
-                    onTap: ()=> Get.toNamed('/post'),
-                  ),
-                )
-              ],
-          )
+      // appBar: AppBar(
+      //   title: Text('일기를 써보아요'),
+      // ),
+      body:  SliderMenuContainer(
+        sliderMenuOpenSize: 200,
+        title: Text(
+          'ddddd',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
+        ),
+        sliderMenu: MenuWidget(
+          onItemClick: (title) {
+            // _key.currentState!.closeDrawer();
+            // setState(() {
+            //   this.title = title;
+            // });
+          },
+        ),
+        sliderMain: Container(
+            child: Column(
+                children: [
+                  Text('작성된 일기가 없습니다!'),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.all(30),
+                    child: InkResponse(
+                      child: Center(child: Text('여기를 눌러 일기를 써보세요!')),
+                      onTap: ()=> Get.toNamed('/post'),
+                    ),
+                  )
+                ],
+            )
+        ),
       )
     );
   }
--- gyeony_diary/lib/screen/post_screen.dart
+++ gyeony_diary/lib/screen/post_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class PostScreen extends StatefulWidget {
   @override
@@ -6,8 +10,64 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+
+  List<_SalesData> data = [
+    _SalesData('Jan', 35),
+    _SalesData('Feb', 28),
+    _SalesData('Mar', 34),
+    _SalesData('Apr', 32),
+    _SalesData('May', 40)
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Syncfusion Flutter chart'),
+        ),
+        body: Column(children: [
+          //Initialize the chart widget
+          SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              // Chart title
+              title: ChartTitle(text: 'Half yearly sales analysis'),
+              // Enable legend
+              legend: Legend(isVisible: true),
+              // Enable tooltip
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <ChartSeries<_SalesData, String>>[
+                LineSeries<_SalesData, String>(
+                    dataSource: data,
+                    xValueMapper: (_SalesData sales, _) => sales.year,
+                    yValueMapper: (_SalesData sales, _) => sales.sales,
+                    name: 'Sales',
+                    // Enable data label
+                    dataLabelSettings: DataLabelSettings(isVisible: true))
+              ]),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            //Initialize the spark charts widget
+            child: SfSparkLineChart.custom(
+              //Enable the trackball
+              trackball: SparkChartTrackball(
+                  activationMode: SparkChartActivationMode.tap),
+              //Enable marker
+              marker: SparkChartMarker(
+                  displayMode: SparkChartMarkerDisplayMode.all),
+              //Enable data label
+              labelDisplayMode: SparkChartLabelDisplayMode.all,
+              xValueMapper: (int index) => data[index].year,
+              yValueMapper: (int index) => data[index].sales,
+              dataCount: 5,
+            ),
+          )
+        ]));
   }
+}
+
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
 }
