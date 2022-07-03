8055f1c2afd4d2b6f0e8423582b5809c2aa58d83
Gyeony95 <rlg1133@naver.com>
2022-07-03 16:37:23 +0900

    초기화

--- plugin_test/example/android/app/src/main/kotlin/com/example/plugin_test_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plugin_test_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- plugin_test/ios/Classes/PluginTestPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PluginTestPlugin : NSObject<FlutterPlugin>
-@end
