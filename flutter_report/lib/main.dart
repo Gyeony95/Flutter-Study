783962a3abf1be6d754677b666d48ff386e658b6
Gyeony95 <rlg1133@naver.com>
2022-07-06 21:42:48 +0900

    feature : 애니메이션 에제 작성

--- flutter_report/lib/main.dart
+++ flutter_report/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_report/pages/01_animation_example_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +12,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FeatureList(),
+        FeatureEnum.animationExample.route: (context) => const AnimationExample(),
+      },
+
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FeatureList extends StatefulWidget {
+  const FeatureList({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<FeatureList> createState() => _FeatureListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _FeatureListState extends State<FeatureList> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: AppBar(title: const Text('플러터 기능 리스트'),),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: FeatureEnum.values.length,
+          itemBuilder : listItem,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget listItem(BuildContext context, int index){
+    var currentFeature = FeatureEnum.values[index];
+    return GestureDetector(
+      onTap: currentFeature.onTap(context),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: const BorderRadius.all(Radius.circular(10))
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        width: double.infinity,
+        height: 70,
+        alignment: Alignment.center,
+        child: Text(currentFeature.name, style: const TextStyle(
+          fontSize: 20,
+        ),),
+      ),
+    );
+  }
+}
+
+
+enum FeatureEnum{
+  animationExample,
+}
+
+extension FeatureEnumExtention on FeatureEnum{
+  VoidCallback onTap(BuildContext context) => (){Navigator.pushNamed(context, route);};
+  String get route{
+    switch(this){
+      case FeatureEnum.animationExample :
+        return '/animation_example';
+    }
+  }
+  String get name{
+    switch(this){
+      case FeatureEnum.animationExample :
+        return '애니메이션 예제';
+    }
   }
 }
---
+++ flutter_report/lib/pages/01_animation_example_page.dart
@@ -0,0 +1,135 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class AnimationExample extends StatefulWidget{
+  const AnimationExample({Key? key}) : super(key: key);
+
+  @override
+  State<AnimationExample> createState() => _AnimationExampleState();
+}
+
+class _AnimationExampleState extends State<AnimationExample>
+    with TickerProviderStateMixin{
+
+  bool isRun = false;
+
+  // 상단 컨테이너 애니메이션
+  late AnimationController controller;
+  late Animation<Offset> topAnimation;
+
+  // 가운데 컨테이너 애니메이션
+  late Animation<Offset> middleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    topAnimation = Tween<Offset>(begin: const Offset(0,0), end: const Offset(0,-1))
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeInCubic));
+
+    middleAnimation = Tween<Offset>(begin: const Offset(0,0), end: const Offset(1,0))
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeInCubic));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('애니메이션 예제'),),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              topContainer(),
+              middleContainer(),
+              bottomContainer(),
+            ],
+          ),
+          Positioned(
+            bottom: 50,
+            right: 20,
+            child: runButton())
+        ],
+      ),
+    );
+  }
+
+  Widget topContainer(){
+    return SlideTransition(
+      position: topAnimation,
+      child: Container(
+        height: 150,
+        decoration: const BoxDecoration(
+          color: Colors.lightBlue,
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(40),)
+        ),
+      ),
+    );
+  }
+
+  Widget middleContainer(){
+    return SlideTransition(
+      position: middleAnimation,
+      child: Container(
+        margin: const EdgeInsets.only(top: 20, left: 10),
+        height: 100,
+        child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Container(
+            width: 100,
+            height: 100,
+            color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bottomContainer(){
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 10,
+          itemBuilder: (context,index) {
+            Animation<double> itemAnimation;
+            itemAnimation = Tween<double>(begin: 1, end: 0)
+                .animate(CurvedAnimation(parent: controller, curve: Interval(0.1 * index, 0.2 + index * 0.1)));
+            return FadeTransition(
+              opacity: itemAnimation,
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: const SizedBox(
+                  width: double.infinity,
+                  height: 70,
+                ),
+              ),
+            );
+          },
+      ),
+    );
+  }
+
+  Widget runButton(){
+    return GestureDetector(
+      onTap: (){
+        if(isRun){
+          controller.reverse();
+        }else{
+          controller.forward();
+        }
+        isRun = !isRun;
+      },
+      child: Container(
+        width: 100,
+        height: 50,
+        color: Colors.amberAccent,
+        alignment: Alignment.center,
+        child: const Text('RUN!!', style: TextStyle(fontSize: 20),),
+      ),
+    );
+  }
+}
